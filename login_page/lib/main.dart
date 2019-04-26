81af8e47bb8eb68a0b7a3f3b496d03286dbb1a2d
Vaibhhav Bisht <vaibhavbisht06@gmail.com>
2019-04-26 17:39:55 +0530

    Final Comit

--- login_page/lib/main.dart
+++ login_page/lib/main.dart
@@ -24,18 +24,16 @@ class _LoginPageState extends State<LoginPage> with SingleTickerProviderStateMix
   Animation<double> _iconAnimation;
 
   @override
-  void initstate(){
+  void initState() {
     super.initState();
-    _iconAnimationController =new AnimationController(
-        vsync: this,
-        duration: new Duration(milliseconds: 500)
+    _iconAnimationController = new AnimationController(
+        vsync: this, duration: new Duration(milliseconds: 900));
+    _iconAnimation = new CurvedAnimation(
+      parent: _iconAnimationController,
+      curve: Curves.bounceOut,
     );
-    _iconAnimation= new CurvedAnimation(
-        parent: _iconAnimationController,
-        curve: Curves.bounceOut
-    );
-    _iconAnimation.addListener(()=>this.setState((){}));
-    _iconAnimationController.forward( );
+    _iconAnimation.addListener(() => this.setState(() {}));
+    _iconAnimationController.forward();
   }
   @override
   Widget build(BuildContext context) {
@@ -46,19 +44,63 @@ class _LoginPageState extends State<LoginPage> with SingleTickerProviderStateMix
         children: <Widget>[
           new  Image(image : new AssetImage('images/girl.jpeg'),
           fit: BoxFit.cover,
-          color: Colors.black45,
+          color: Colors.black54,
           colorBlendMode: BlendMode.darken,
           ),
-          new Column(
+    new Theme(
+    data: new ThemeData(
+    brightness: Brightness.dark,
+    inputDecorationTheme: new InputDecorationTheme(
+    // hintStyle: new TextStyle(color: Colors.blue, fontSize: 20.0),
+    labelStyle:
+    new TextStyle(color: Colors.tealAccent, fontSize: 25.0),
+    )),
+    isMaterialAppTheme: true,
+          child: new Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
               new FlutterLogo(
                 size: _iconAnimation.value*100,
+              ),
+              new Container(
+                padding: const EdgeInsets.all(40.0),
+                child: new Form(
+                  autovalidate: true,
+                  child: new Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: <Widget>[
+                      new TextFormField(
+                        decoration: new InputDecoration(
+                            labelText: "Enter Email", fillColor: Colors.blue,),
+                        keyboardType: TextInputType.emailAddress,
+                      ),
+                      new TextFormField(
+                        decoration: new InputDecoration(
+                          labelText: "Enter Password",
+                        ),
+                        obscureText: true,
+                        keyboardType: TextInputType.text,
+                      ),
+                      new Padding(
+                        padding: const EdgeInsets.only(top: 60.0),
+                      ),
+                      new MaterialButton(
+                        height: 50.0,
+                        minWidth: 150.0,
+                        color: Colors.green,
+                        splashColor: Colors.teal,
+                        textColor: Colors.white,
+                        child: new Text("Login",style: TextStyle(fontSize: 20.0,decorationStyle: TextDecorationStyle.wavy,letterSpacing: 2.0,fontStyle: FontStyle.italic ),),
+                        onPressed: () {},
+                      )
+                    ],
+                  ),
+                ),
               )
             ],
           )
-        ],
-      ),
+    ),
+  ] ),
     );
   }
 }
